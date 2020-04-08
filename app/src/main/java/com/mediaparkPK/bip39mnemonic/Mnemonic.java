a4fea351715200033ada3633c7548cdeafd82044
zaryab <zaryabshakir99@gmail.com>
2020-04-08 22:24:08 +0500

    Mnemonic implementation

---
+++ app/src/main/java/com/mediaparkPK/bip39mnemonic/Mnemonic.java
@@ -0,0 +1,48 @@
+package com.mediaparkPK.bip39mnemonic;
+
+import android.text.TextUtils;
+
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.util.ArrayList;
+
+import static com.mediaparkPK.bip39mnemonic.utils.Bip39Utils.bytesToHex;
+import static com.mediaparkPK.bip39mnemonic.utils.MnemonicUtils.pbkdf2;
+
+public class Mnemonic {
+
+    public String entropy;
+    public int wordsCount;
+    public ArrayList<Integer> wordsIndex;
+    public ArrayList<String> words;
+    public ArrayList<String> rawBinaryChunks;
+
+
+    public Mnemonic(){
+        this.entropy = "";
+        this.wordsCount = 0;
+        this.wordsIndex= new ArrayList<>();
+        this.words=new ArrayList<>();
+        this.rawBinaryChunks= new ArrayList<>();
+    }
+
+
+    public  Mnemonic(String entropy) {
+        this.entropy = entropy;
+        this.wordsCount = 0;
+        this.wordsIndex= new ArrayList<>();
+        this.words=new ArrayList<>();
+        this.rawBinaryChunks= new ArrayList<>();
+    }
+    public String generateSeed(String passphrase, int bytes) throws InvalidKeySpecException, NoSuchAlgorithmException {
+        return bytesToHex(pbkdf2(
+                TextUtils.join(" ",words).toCharArray(),
+                ("mnemonic"+passphrase).getBytes(),
+                2048,
+                bytes
+        ));
+    }
+
+
+
+}
---
+++ app/src/main/java/com/mediaparkPK/bip39mnemonic/utils/MnemonicUtils.java
@@ -0,0 +1,27 @@
+package com.mediaparkPK.bip39mnemonic.utils;
+
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+
+public class MnemonicUtils {
+    private static final char[] HEX_ARRAY = "0123456789ABCDEF".toCharArray();
+    private static final String algorithm = "PBKDF2WithHmacSHA512";
+    private static final  String bytesToHex(byte[] bytes) {
+        char[] hexChars = new char[bytes.length * 2];
+        for (int j = 0; j < bytes.length; j++) {
+            int v = bytes[j] & 0xFF;
+            hexChars[j * 2] = HEX_ARRAY[v >>> 4];
+            hexChars[j * 2 + 1] = HEX_ARRAY[v & 0x0F];
+        }
+        return new String(hexChars);
+    }
+    public static byte[] pbkdf2(char[] password, byte[] salt, int iterations,int bytes) throws NoSuchAlgorithmException, InvalidKeySpecException
+    {
+        PBEKeySpec spec = new PBEKeySpec(password, salt, iterations,bytes);
+        SecretKeyFactory skf = SecretKeyFactory.getInstance(algorithm);
+        return skf.generateSecret(spec).getEncoded();
+    }
+}
