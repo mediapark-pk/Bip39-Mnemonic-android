8cf5930b95d339493da0834b622277a56a91daa7
zaryab <zaryabshakir99@gmail.com>
2020-04-09 20:27:29 +0500

    Exceptions Handled

--- app/src/main/java/com/mediaparkPK/bip39mnemonic/utils/WordListUtils.java
+++ app/src/main/java/com/mediaparkPK/bip39mnemonic/utils/WordListUtils.java
@@ -2,11 +2,13 @@ package com.mediaparkPK.bip39mnemonic.utils;
 
 import android.content.Context;
 
+import com.mediaparkPK.bip39mnemonic.exceptions.WordListException;
+
 import java.io.IOException;
 import java.io.InputStream;
 
 public class WordListUtils {
-    public static String getFileContent(String language, Context context) {
+    public static String getFileContent(String language, Context context) throws WordListException {
         try {
             InputStream inputStreamReader = context.getAssets().open(language + ".txt");
             int size = inputStreamReader.available();
@@ -15,7 +17,7 @@ public class WordListUtils {
             inputStreamReader.close();
             return new String(buffer);
         } catch (IOException e) {
+            throw new WordListException("BIP39 wordlist for  not found or is not readable"+language);
         }
-        return null;
     }
 }
