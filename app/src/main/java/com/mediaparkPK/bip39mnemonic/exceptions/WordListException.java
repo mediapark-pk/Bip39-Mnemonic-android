704402350de2b5e9c1324268e84410e65606f420
zaryab <zaryabshakir99@gmail.com>
2020-04-09 20:26:04 +0500

    Wordlist Exception Added

---
+++ app/src/main/java/com/mediaparkPK/bip39mnemonic/exceptions/WordListException.java
@@ -0,0 +1,8 @@
+package com.mediaparkPK.bip39mnemonic.exceptions;
+
+public class WordListException extends Exception {
+
+    public WordListException(String exception){
+        super(exception);
+    }
+}
