00b8c2e12d8c7197f82483ae421f95585f40e3b9
zaryab <zaryabshakir99@gmail.com>
2020-04-09 20:26:28 +0500

    Bip39 Exception Added

---
+++ app/src/main/java/com/mediaparkPK/bip39mnemonic/exceptions/BIP39Exception.java
@@ -0,0 +1,7 @@
+package com.mediaparkPK.bip39mnemonic.exceptions;
+
+public class BIP39Exception extends Exception {
+    public BIP39Exception(String exception){
+        super(exception);
+    }
+}
